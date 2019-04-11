4e0cee51f9e340061d6cf356d4a6d740aad5ccc9
anuj-k-s <anujkumarsoni1993@gmail.com>
2019-04-11 10:37:07 +0530

    singleton class completed

--- SingletonExample/aks.singleton/SingletonClass.java
+++ SingletonExample/aks.singleton/SingletonClass.java
@@ -1,4 +1,21 @@
 
 public class SingletonClass {
-
+	/*
+	 * public SingletonClass() { System.out.println("Inside Singleton Class");//
+	 * TODO Auto-generated constructor stub }
+	 */
+	private static SingletonClass singletonClassInstance = null;
+	
+	private SingletonClass() {
+		System.out.println("Inside private Constructor");
+	}
+	
+	public static SingletonClass getInstance(){
+		if(singletonClassInstance == null) {
+			singletonClassInstance = new SingletonClass();
+			return singletonClassInstance;
+		}
+		return singletonClassInstance;
+	}
+	
 }
--- SingletonExample/aks.singleton/Test.java
+++ SingletonExample/aks.singleton/Test.java
@@ -1,4 +1,23 @@
 
 public class Test {
+	public static void main(String[] args) {
 
+		SingletonClass class1 = SingletonClass.getInstance();
+		System.out.println("Hashcode for 1st Object:- " + class1.hashCode());
+
+		SingletonClass class2 = SingletonClass.getInstance();
+		System.out.println("Hashcode for 2st Object:- " + class2.hashCode());
+
+		System.out.println("Result of .equals methods on both object:- " + class1.equals(class2));
+		System.out.println(class1 == class2);
+
+		/*
+		 * We can use == operators for reference comparison (address comparison) and
+		 * .equals() method for content comparison. In simple words, == checks if both
+		 * objects point to the same memory location whereas .equals() evaluates to the
+		 * comparison of values in the objects. If a class does not override the equals
+		 * method, then by default it uses equals(Object o) method of the closest parent
+		 * class that has overridden this method
+		 */
+	}
 }
